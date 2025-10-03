86691e560c70b626aa06c02ed106645d496a7f2d
monjurulalif <monjurulalif.info@gmail.com>
2025-10-03 22:21:22 +0600

    alif

---
+++ DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+
+    public static void main(String[] args) {
+
+        boolean b;
+        char c;
+        short s;
+        int i;
+        float f;
+        double d;
+
+        b = true;
+        System.out.println("b = " + b);
+
+    }
+}
