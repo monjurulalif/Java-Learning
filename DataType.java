bb6ab9829e35562a192e31659cd5dd979845b7ac
monjurulalif <monjurulalif.info@gmail.com>
2025-10-04 20:55:44 +0600

    a

--- DataType.java
+++ DataType.java
@@ -4,13 +4,21 @@ public class DataType {
 
         boolean b;
         char c;
-        short s;
-        int i;
-        float f;
-        double d;
+        int s = 246345;
+        int i = 123456;
+        float f = 323.45f;
+        double d = 12345.6789;
 
         b = true;
-        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+
+
+
+
 
     }
 }
+
