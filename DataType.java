d049b3256451feaedd3da73f2f2ffed09af9cc9a
monjurulalif <monjurulalif.info@gmail.com>
2025-10-04 21:17:20 +0600

    A2

--- DataType.java
+++ DataType.java
@@ -1,3 +1,4 @@
+/*
 public class DataType {
 
     public static void main(String[] args) {
@@ -15,10 +16,44 @@ public class DataType {
         System.out.println("f = " + f);
         System.out.println("d = " + d);
 
+    }
+} */
 
+
+      /*
+ * Assignment-2 (Variable and Data type)
+ * step 1: create a class called Product
+ * step 2: create a main method
+ * step 3: declare variables: id, title, price, description, category
+ * step 4: assign the following data in main method
+101,iphone15,1895 euros,perfect product with best image quality, phone,
+* step 5: print the data
+ */
+
+public class DataType {  //Assignment 2
+
+    public static void main(String[] args) {
+
+        String itle = "iphone17_pro_max";
+		String price = "18950 euros";
+		String description = "perfect product with best image quality";
+		String category = "phone";
+		
+        System.out.println("itle: " + itle);
+        System.out.println("price: " + price);
+        System.out.println("description: " + description);
+        System.out.println("category: " + category);
 
 
 
     }
-}
+
+    }
+
+
+
+
+
+
+
 
